06d8841017d29db5f539338f9439baa716fe2bb3
tunas <tuna9x.it@gmail.com>
2025-02-03 16:08:25 +0700

    Add link to logo

--- vite-ecom-shop/src/components/common/Header.jsx
+++ vite-ecom-shop/src/components/common/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 const Header = () => {
   return (
     <header className="bg-amber-300">
       <div className="container mx-auto">
         <div className=" flex justify-between py-3 px-2">
-          LOGO
+          <NavLink className="home link" to="/">
+            LOGO
+          </NavLink>
           <p className="text-md hover:underline ">LOGIN/REGISTER</p>
         </div>
       </div>
